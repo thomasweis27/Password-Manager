9eac57835504263421d78fe31e12d9de836c0b7e
Horaxxion <w.cale.morrison@outlook.com>
2024-04-16 10:10:58 -0400

    modified:   __pycache__/Decrypt.cpython-311.pyc 	modified:   __pycache__/Encrypt.cpython-311.pyc 	modified:   __pycache__/accessCredentialSets.cpython-311.pyc 	modified:   __pycache__/addCredentialSet.cpython-311.pyc 	modified:   __pycache__/createAccount.cpython-311.pyc 	modified:   __pycache__/credentialsMainScreen.cpython-311.pyc 	modified:   __pycache__/editAccount.cpython-311.pyc 	modified:   __pycache__/viewCredentialSet.cpython-311.pyc 	modified:   credentials.txt 	modified:   credentialsMainScreen.py 	modified:   users.txt

--- credentialsMainScreen.py
+++ credentialsMainScreen.py
@@ -26,19 +26,26 @@ def credentialsMainScreen(enteredPassword, currentHash, oldHash, login):
     frmSideBar = tk.Frame(window, relief = tk.RAISED, bd = 2, height=window.winfo_height())
     frmSideBar.grid(row=0, column=0)
 
-    mainBtn = tk.Button(master = frmSideBar, text = "Main", width=10, height=2)
-    searchBtn = tk.Button(master = frmSideBar, text = "Search", height=2, command=lambda: accessCredentialSets(currentHash, enteredPassword, window))#changed to add encryption!!!!!!
+    frmRecentPin = tk.Frame(window, relief = tk.RAISED, bd = 2, height=window.winfo_height())
+    frmRecentPin.grid(row=0, column=1)
+
     addBtn = tk.Button(master = frmSideBar, text = "Add", height=2, command=lambda: addCredentialSet(currentHash, enteredPassword, window))
-    editBtn = tk.Button(master = frmSideBar, text = "Edit", height=2)
-    accountBtn = tk.Button(master = frmSideBar, text = "Button", height=2)
+    searchBtn = tk.Button(master = frmSideBar, text = "Search", height=2, command=lambda: accessCredentialSets(currentHash, enteredPassword, window))#changed to add encryption!!!!!!
     logoutBtn = tk.Button(master = frmSideBar, text = "Logout", height=2, command=lambda: logout(login, window))
 
-    mainBtn.grid(row=0, column=0, sticky="ew")
+    recentLabel = tk.Label(master=frmRecentPin, text="Recently Searched Credentials")
+    recentListbox = tk.Listbox(master=frmRecentPin, height=10, width=20)
+    pinnedLabel = tk.Label(master=frmRecentPin, text="Pinned Credentials")
+    pinnedListbox = tk.Listbox(master=frmRecentPin, height=10, width=20)
+
+    recentLabel.grid(row=0, column=0)
+    recentListbox.grid(row=1, column=0)
+    pinnedLabel.grid(row=0, column=1)
+    pinnedListbox.grid(row=1, column=1)
+
+    addBtn.grid(row=0, column=0, sticky="ew")
     searchBtn.grid(row=1, column=0, sticky="ew")
-    addBtn.grid(row=2, column=0, sticky="ew")
-    editBtn.grid(row=3, column=0, sticky="ew")
-    accountBtn.grid(row=4, column=0, sticky="ew")
-    logoutBtn.grid(row=5, column=0, sticky="ew")
+    logoutBtn.grid(row=2, column=0, sticky="ew")
 
     window.mainloop()
 
